39fa51515caa69c22ddcfc29c9b11b2d19da3eac
ALexander4295502 <yuanzhengstl@gmail.com>
2017-12-03 17:25:38 -0600

    create CRUD CBVs for team app

--- djangoal/views.py
+++ djangoal/views.py
@@ -1,10 +1,5 @@
-from django.shortcuts import render
 from django.views.generic import View, TemplateView
 from django.http import HttpResponse
-
-
-def home(request):
-    return render(request, 'home.html')
 
 
 class HomeView(TemplateView):
--- teams/views.py
+++ teams/views.py
@@ -1,13 +1,31 @@
-from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import (
+    ListView, DetailView,
+    CreateView, UpdateView, DeleteView
+)
 
 from . import models
 
 
-def team_list(request):
-    teams = models.Team.objects.all()
-    return render(request, 'teams/team_list.html', {'teams': teams})
+class TeamListView(ListView):
+    model = models.Team
+    context_object_name = 'teams'
 
 
-def team_detail(request, pk):
-    team = get_object_or_404(models.Team, pk=pk)
-    return render(request, 'teams/team_detail.html', {'team': team})
+class TeamDetailView(DetailView):
+    model = models.Team
+
+
+class TeamCreateView(CreateView):
+    fields = ("name", "practice_location", "coach")
+    model = models.Team
+
+
+class TeamUpdateView(UpdateView):
+    fields = ("name", "practice_location", "coach")
+    model = models.Team
+
+
+class TeamDeleteView(DeleteView):
+    model = models.Team
+    success_url = reverse_lazy('teams:list')
